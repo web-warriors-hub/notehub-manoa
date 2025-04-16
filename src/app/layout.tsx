0bb64e4cf5f08ef38bc0860f600882750742fd5b
cat-mb <115598926+cat-mb@users.noreply.github.com>
2025-04-15 18:02:49 -1000

    transferring layout to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body className={classString}>
         <Providers>
             <NavBar />
-              <LandingBackground />
               {children}
             <Footer />
         </Providers>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Col, Container, Image, Row } from 'react-bootstrap';
+import LandingBackground from '../components/LandingPage/LandingBackground';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    
+     <LandingBackground />
   </main>
 );
 
