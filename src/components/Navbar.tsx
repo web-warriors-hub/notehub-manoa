9d7f47986217ada8d453bfc45fb07f54b536c01c
Milan Bukovics <milan.bukovics@gmail.com>
2025-04-15 18:08:16 -1000

    Update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const NavBar: React.FC = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">digits</Navbar.Brand>
+        <Navbar.Brand href="/">Notehub Manoa</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
