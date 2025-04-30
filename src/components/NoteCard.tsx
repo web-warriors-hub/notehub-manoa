b36f2d09e3f6506f4d6d30a4f1bf9c125cde1c9d
Landon Kihe <lkihe@hawaii.edu>
2025-04-29 19:38:26 -1000

    Update NoteCard.tsx

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -12,6 +12,8 @@ const NoteCard = ({ note }: { note: Note }) => (
         {note.class}
         -
         {note.semester}
+        -Created:
+        {new Date(note.createdAt).toLocaleDateString()}
       </Card.Subtitle>
     </Card.Header>
     <Card.Body>
