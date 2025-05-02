a5808074c101c6ccf5cdc1e11066e76621932a7a
Andrea <133811058+andreamurillomtz@users.noreply.github.com>
2025-05-01 19:12:09 -1000

    Fixed design of page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,22 @@ const Footer = () => (
   <footer className="py-5 custom-footer text-center">
     Department of Information and Computer Sciences
     <br />
-    University of Hawaii
+    University of Hawaii at Manoa
     <br />
     Honolulu, HI 96822
     <br />
+    <br />
+    Organization:{' '}
+    <a
+      href="https://web-warriors-hub.github.io"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-decoration-underline text-primary"
+    >
+      https://web-warriors-hub.github.io
+    </a>
+    <br />
+    <i>Developers: Andrea Murillo, Catalina Barnes, Landon Kihe, Milan Bukovics, Nalani</i>
   </footer>
 );
 
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -5,41 +5,38 @@ import Link from 'next/link';
 import { Card, Col, Row } from 'react-bootstrap';
 
 const NoteCard = ({ note }: { note: Note }) => (
-  <Card className="h-100 w-100">
-    <Card.Header>
-      <Card.Title>{note.title}</Card.Title>
-      <Card.Subtitle>
-        {note.class}
-        -
-        {note.semester}
-        {/* -Created:
-        {new Date(note.createdAt).toLocaleDateString()} */}
+  <Card className="h-100 w-100 border rounded-4 shadow-sm p-2 bg-white">
+    <Card.Header className="bg-white border-bottom-0">
+      <Card.Title className="mb-1 fs-2 fw-semibold text-dark">{note.title}</Card.Title>
+      <Card.Subtitle className="text-muted text-sm">
+        {note.class} — {note.semester}
       </Card.Subtitle>
     </Card.Header>
-    <Card.Body>
-      <Card.Text>
-        <strong>Professor:</strong>
+
+    <Card.Body className="pt-3">
+      <Card.Text className="text-notecard small">
+        <strong className="text-dark">Professor: </strong>
         {note.professor}
         <br />
-        <strong>Department:</strong>
+        <strong className="text-dark">Department: </strong>
         {note.department}
         <br />
-        <strong>Description:</strong>
+        <strong className="text-dark">Description: </strong>
         {note.description}
         <br />
-        <strong>Document:</strong>
-        <Link href={note.documentLink} target="_blank">
+        <strong className="text-dark">Document: </strong>{' '}
+        <Link href={note.documentLink} target="_blank" className="text-success text-decoration-underline">
           View
         </Link>
       </Card.Text>
     </Card.Body>
-    <Card.Footer>
+
+    <Card.Footer className="bg-white border-top-0">
       <Row>
         <Col>
-          <small>
-            Uploaded by
-            {note.owner}
-          </small>
+          <div className="text-muted" style={{ fontSize: '1.2rem' }}>
+            Uploaded by {note.owner}
+          </div>
         </Col>
       </Row>
     </Card.Footer>
