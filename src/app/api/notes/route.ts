f3f34a2ac0fec33d51b03764c265064187c113cc
Landon Kihe <lkihe@hawaii.edu>
2025-05-06 18:09:56 -1000

    More fixes

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -3,6 +3,8 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
+export const dynamic = 'force-dynamic';
+
 const prisma = new PrismaClient();
 
 export async function GET() {
